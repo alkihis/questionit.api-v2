19d53d165d2a5c37764db5e02d895b4328f87646
Louis Béranger <beranger.louis.bio@gmail.com>
2022-08-20 16:47:09 +0200

    feat: Updated packages, new REPL

--- src/repl.ts
+++ src/repl.ts
@@ -9,22 +9,23 @@ import { DataSource } from 'typeorm';
 
 async function bootstrap() {
   const server = await repl(AppModule);
+  const ctx = server.context;
 
   // Define REPL available variables
-  server.context.db = server.context.get(server.context.AppService).db; // Extract db from AppService instance.
-  server.context.redis = RedisService;
-  server.context.model = Object.fromEntries(ENTITIES.map(e => [e.name, e]));
-  server.context.typeorm = typeorm;
-  server.context.asLogged = async (user: User | number, callback: Function) => {
+  ctx.db = ctx.get(ctx.AppService).db; // Extract db from AppService instance.
+  ctx.redis = RedisService;
+  ctx.model = Object.fromEntries(ENTITIES.map(e => [e.name, e]));
+  ctx.typeorm = typeorm;
+  ctx.asLogged = async (user: User | number, callback: Function) => {
     if (typeof user === 'number') {
-      user = await (server.context.db as DataSource).getRepository(User).findOneByOrFail({ id: user });
+      user = await (ctx.db as DataSource).getRepository(User).findOneByOrFail({ id: user });
     }
 
     const req: any = { user };
     const res: any = {};
-    const ctx = new RequestContext(req, res);
+    const reqCtx = new RequestContext(req, res);
 
-    return RequestContext.cls.run(ctx, () => callback());
+    return RequestContext.cls.run(reqCtx, () => callback());
   };
 
   // EXAMPLE USAGE: Obtain list of followings of 'Alkihis'
